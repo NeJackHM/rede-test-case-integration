f735502298c7c91bb80a8fb28d9b202419f41d76
nelson huaman mendez <nelson_trq@hotmail.com>
2024-08-06 03:34:02 -0300

    add readme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PersonList from './Components/Person/PersonList';
-// import EditPersonPage from './Components/Person/EditPersonPage';
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<PersonList />} />
-          {/* <Route path="/edit-person/:personId" element={<EditPersonPage />} /> */}
         </Routes>
       </div>
     </Router>
